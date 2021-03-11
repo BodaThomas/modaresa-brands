cb5d2925bab62cdfccc11c75fd1877175bb78ac8
Thomas Boda <boda.thomas@orange.fr>
2021-03-11 15:36:35 +0100

    Add the ESLint configuration on both sides

---
+++ backend/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'warn',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'eol-last': [
+            'warn',
+            'always'
+        ]
+    }
+}
